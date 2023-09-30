c811b17634795ed33e0562a62a2707fbc2ae11a8
Youssuf <youssufhichri@cmail.carleton.ca>
2023-09-30 14:53:50 -0400

    foo2

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -9,27 +9,48 @@ function Navbar() {
 
   return (
     <div className="navbar">
+      {/* <ul>
+        <li>
+          <div>
+            <div className="navbar__logo">
+              <Link to="/">Store Logo</Link>
+            </div>
+
+            <div className="navbar__account">
+              {loggedIn() === false ? (
+                <>
+                <li style={{float: 'right' }}><Link to="/sign-up" >Sign Up</Link></li>
+                  
+                <li style={{float: 'right' }}><Link to="/login">Login</Link></li>
+                </>
+              ) : (
+                // <Link to="logout()">Logout</Link>
+                <button onClick={() => logout()}>Logout</button>
+              )}
+            </div>
+          </div>
+        </li>
+      </ul> */}
+
+      
       <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">News</a></li>
-        <li><a href="">Contact</a></li>
-        <li><a href="">About</a></li>
-      </ul>
 
+        <li><Link to="/">Store Logo</Link></li>
 
-      <div className="navbar__logo">
-        <Link to="/">Store Logo</Link>
-      </div>
-      <div className="navbar__account">
         {loggedIn() === false ? (
-          <>
-            <button onClick={() => navigate("/sign-up")}>Sign Up</button>
-            <button onClick={() => navigate("/login")}>Login</button>
-          </>
-        ) : (
-          <button onClick={() => logout()}>Logout</button>
-        )}
-      </div>
+            <>
+            <li style={{float: 'right' }}><Link to="/sign-up" >Sign Up</Link></li>
+              
+            <li style={{float: 'right' }}><Link to="/login">Login</Link></li>
+            </>
+          ) : (
+            // <Link to="logout()">Logout</Link>
+            <button onClick={() => logout()}>Logout</button>
+          )}
+
+      </ul>
+      
+      
     </div>
   );
 }
