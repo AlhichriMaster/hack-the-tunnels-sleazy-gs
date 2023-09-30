33602d87685a8524c430003e270602e321110c81
AlhichriMaster <74574079+AlhichriMaster@users.noreply.github.com>
2023-09-30 17:45:55 +0000

    foo

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -9,6 +9,14 @@ function Navbar() {
 
   return (
     <div className="navbar">
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">News</a></li>
+        <li><a href="">Contact</a></li>
+        <li><a href="">About</a></li>
+      </ul>
+
+
       <div className="navbar__logo">
         <Link to="/">Store Logo</Link>
       </div>
---
+++ client/src/pages/Error/Error.tsx
@@ -0,0 +1,10 @@
+function Error() {
+    return (
+        <div className="error-page">
+          <h1>404: Not Found 💀</h1>
+        </div>
+    );
+  }
+  
+  export default Error;
+  
